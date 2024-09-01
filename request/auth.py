0766a65b0549686f9568d41efdf164e41e81edf2
cyziek <krzysztof112997@gmail.com>
2024-09-02 00:57:27 +0200

    todo: fix saving cookies after auth

--- request/auth.py
+++ request/auth.py
@@ -28,10 +28,62 @@ def refresh_token():
             "\n\n\nTwoje cookiesy wygasły (ping-bearer timeout)! Zaloguj się w przeglądarce i wyeksportuj pliki cookies do pliku request/COOKIES.txt\n\n\n")
     ##
 
-
     if response.status_code == 200 and response.json()['bearerToken'] != False:
-        bearer_value = response.json()
-        bearer_value = bearer_value['bearerToken']
-        with open("request/bearer.txt", "w") as bearer_file:
-            bearer_file.write(bearer_value)
-        return
+        save_headers_from_response(response)
+        # save_cookies_from_response(response)
+
+
+def save_headers_from_response(response):
+    bearer_value = response.json()
+    bearer_value = bearer_value['bearerToken']
+
+    response_cookies = response.cookies
+    with open("request/bearer.txt", "w") as bearer_file:
+        bearer_file.write(bearer_value)
+    return
+
+
+def save_cookies_from_response(response):
+    new_cookies = {}
+
+    with open('COOKIES.TXT', 'r') as file:
+        existing_cookies = json.load(file)
+    for cookie in response.cookies:
+        new_cookies[cookie.name] = cookie.value
+        upsert_cookie(existing_cookies, cookie.domain, cookie.name, cookie.value, cookie.path, cookie.expires, cookie.secure)
+    #
+    # for name, value in new_cookies.items():
+    #     upsert_cookie(existing_cookies, domain, name, value, path)
+
+    # with open('cookies.json', 'w') as f:
+    #     json.dump(existing_cookies, f, indent=4)
+    #
+    # for name, value in new_cookies.items():
+    #     upsert_cookie(existing_cookies, domain, name, value, path)
+
+
+def upsert_cookie(existing_cookies, domain, name, value, path, expirationDate, secure):
+    found = False
+    for cookie in existing_cookies:
+        if cookie['domain'] == domain and cookie['name'] == name and cookie['path'] == path:
+            cookie['value'] = value
+            found = True
+            break
+    if not found:
+        # If the cookie does not exist, append a new one
+        existing_cookies.append({
+            "domain": domain,
+            "expirationDate": expirationDate,
+            "hostOnly": False,
+            "httpOnly": False,
+            "name": name,
+            "path": path,
+            "sameSite": None,
+            "secure": secure,
+            "session": True,
+            "storeId": None,
+            "value": value
+        })
+
+    with open('COOKIES.TXT', 'w') as f:
+        json.dump(existing_cookies, f, indent=4)
