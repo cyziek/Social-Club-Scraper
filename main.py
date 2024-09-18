31ea5de531f5f5a11961111f6da87d97abba8e06
cyziek <krzysztof112997@gmail.com>
2024-09-18 17:10:58 +0200

    last fixes

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from request.cookies import get_cookies_as_header
 from stats.player_stats import *
 
 if __name__ == '__main__':
-    # crew_members_import()
+    crew_members_import()
     with open("./output/crew_members.txt", 'r') as file:
         all_users = file.read().splitlines()
 
-        while load_progress() <= len(all_users)-1:
+        while load_progress() <= len(all_users) - 1:
             user = all_users[load_progress()]
 
             last_seen_date = get_user_last_seen_date(user)
@@ -21,8 +21,9 @@ if __name__ == '__main__':
             else:
                 user_platforms = ";;;;;"
             with open("./output/player_stats.txt", 'a') as file_last_seen:
-                file_last_seen.write(user + ";" + last_seen_date + ";"+ user_platforms +"\n")
+                file_last_seen.write(user + ";" + last_seen_date + ";" + user_platforms + "\n")
                 print(user + ";" + last_seen_date + ";" + user_platforms + "\n")
-            save_progress(load_progress()+1)
+            save_progress(load_progress() + 1)
             time.sleep(10)
-
+        if load_progress() == len(all_users) - 1:
+            save_progress(0)
